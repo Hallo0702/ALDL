491abbe19b5d924961d627f0b80bcfe419256d1f
최진합 <pppooo95@naver.com>
2022-09-19 13:25:19 +0900

    feat - 글로벌 폰트 패밀리 추가

--- client/src/pages/dummy.tsx
+++ client/src/pages/dummy.tsx
@@ -18,6 +18,45 @@ const Dummy: NextPage = ({}) => {
     };
     fetch();
   }, []);
-  return <div>dummy</div>;
+  return (
+    <>
+      <div className="font-custom font-light text-xs">
+        font-custom font-light text-xs 폰트
+      </div>
+      <div className="font-custom font-medium text-sm">
+        font-custom font-medium text-sm 폰트
+      </div>
+      <div className="font-custom font-regular text-base">
+        font-custom font-regular text-base 폰트
+      </div>
+      <div className="font-custom font-bold text-lg">
+        font-custom font-bold text-lg 폰트
+      </div>
+      <div className="font-custom font-regular text-lg">
+        font-custom font-regular text-lg 폰트
+      </div>
+      <div className="font-custom font-regular text-xl">
+        font-custom font-regular text-xl 폰트
+      </div>
+      <div className="font-custom font-regular text-2xl">
+        font-custom font-regular text-2xl 폰트
+      </div>
+      <div className="font-custom font-regular text-3xl">
+        font-custom font-regular text-3xl 폰트
+      </div>
+      <div className="font-custom font-light text-4xl">
+        font-custom font-light text-4xl 폰트
+      </div>
+      <div className="font-custom font-regular text-4xl">
+        font-custom font-regular text-4xl 폰트
+      </div>
+      <div className="font-custom font-medium text-4xl">
+        font-custom font-medium text-4xl 폰트
+      </div>
+      <div className="font-custom font-bold text-4xl">
+        font-custom font-bold text-4xl 스포카 한 산스 네오
+      </div>
+    </>
+  );
 };
 export default Dummy;
