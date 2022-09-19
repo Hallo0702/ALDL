ea8a2a6f7349d2127faf5370c94ea2df500b5cce
최진합 <pppooo95@naver.com>
2022-09-19 10:41:09 +0900

    test - API 통신 테스트 위한 dummy 페이지 작성

---
+++ client/src/pages/dummy.tsx
@@ -0,0 +1,23 @@
+import type { NextPage } from 'next';
+import axios, { Axios, AxiosError } from 'axios';
+import { useEffect } from 'react';
+
+const Dummy: NextPage = ({}) => {
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+        const res = await axios.get(
+          'http://j7c207.p.ssafy.io:8088/emailduplicate?email=asfasd'
+        );
+        console.log(res);
+      } catch (err) {
+        if (axios.isAxiosError(err)) {
+          console.log(err.response?.data);
+        }
+      }
+    };
+    fetch();
+  }, []);
+  return <div>dummy</div>;
+};
+export default Dummy;
