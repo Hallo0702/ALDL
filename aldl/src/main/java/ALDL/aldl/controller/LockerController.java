1ca3fc48cc847483b412a380ee367cb4e2874210
positivehun <jihun3104@gmail.com>
2022-09-20 14:35:57 +0900

    feat:비밀번호수정 생성

--- aldl/src/main/java/ALDL/aldl/controller/LockerController.java
+++ aldl/src/main/java/ALDL/aldl/controller/LockerController.java
@@ -31,16 +31,20 @@ public class LockerController {
         String nickname = info.get("nickname");
         String location_x = info.get("location_x");
         String location_y = info.get("location_y");
-        if (background == ""||background ==null||
-                design == ""||design==null||
-                nickname==""||nickname==null||
-                location_x==""||location_x==null||
-                location_y==""||location_y==null
-        ){
-            return ResponseEntity.status(400).body("유효하지 않은 값");
+        try{
+            if (background == ""||background ==null||
+                    design == ""||design==null||
+                    nickname==""||nickname==null||
+                    location_x==""||location_x==null||
+                    location_y==""||location_y==null
+            ){
+                return ResponseEntity.status(400).body("유효하지 않은 값");
+            }
+            lockerService.saveLocker(background,design,nickname,location_x,location_y);
+            return ResponseEntity.status(200).body("자물쇠등록완료");
+        }catch(Exception e){
+            return ResponseEntity.status(500).body("에러발생");
         }
-        lockerService.saveLocker(background,design,nickname,location_x,location_y);
-        return ResponseEntity.status(200).body("자물쇠등록완료");
    }
 
     @ApiOperation(value = "자물쇠 소유자 등록")
@@ -50,37 +54,49 @@ public class LockerController {
 
         String email = info.get("email");
         String lockerHash = info.get("lockerHash");
-        if(email==""||email==null||
-        lockerHash==""||lockerHash==null){
-            return ResponseEntity.status(400).body("유효하지 않은 값");
+        try{
+            if(email==""||email==null||
+            lockerHash==""||lockerHash==null){
+                return ResponseEntity.status(400).body("유효하지 않은 값");
 
+            }
+            if(lockerOwnerService.findlocker(email,lockerHash)==false){
+                return ResponseEntity.status(400).body("이미 등록된 자물쇠");
+            }
+            lockerOwnerService.saveLockerOwner(email,lockerHash);
+            return ResponseEntity.status(200).body("자물쇠 저장 완료");
+        }catch(Exception e){
+            return ResponseEntity.status(500).body("에러발생");
         }
-        if(lockerOwnerService.findlocker(email,lockerHash)==false){
-            return ResponseEntity.status(400).body("이미 등록된 자물쇠");
-        }
-        lockerOwnerService.saveLockerOwner(email,lockerHash);
-        return ResponseEntity.status(200).body("자물쇠 저장 완료");
     }
 
     @ApiOperation(value = "장소별 자물쇠 전체 반환",response = Locker.class)
     @CrossOrigin(origins = "*")
     @GetMapping("/backgroundlocker")
     public ResponseEntity<?> allLocker(@RequestParam String background){
-        if (background==null||background==""){
-            return ResponseEntity.status(400).body("유효하지 않는 정보");
+        try{
+            if (background==null||background==""){
+                return ResponseEntity.status(400).body("유효하지 않는 정보");
+            }
+            List<Locker> lockers = lockerService.backgroundLocker(background);
+            return ResponseEntity.status(200).body(lockers);
+        }catch(Exception e){
+            return ResponseEntity.status(500).body("에러발생");
         }
-        List<Locker> lockers = lockerService.backgroundLocker(background);
-        return ResponseEntity.status(200).body(lockers);
 
     }
     @ApiOperation(value = "사용자가 보유중인 자물쇠 반환",response = LockerOwner.class)
     @CrossOrigin(origins = "*")
     @GetMapping("/mylockers")
     public ResponseEntity<?> mylockers(@RequestParam String email){
-        if (email==""||email==null){
-            return ResponseEntity.status(400).body("유효하지 않는 정보");
+        try{
+            if (email==""||email==null){
+                return ResponseEntity.status(400).body("유효하지 않는 정보");
+            }
+            return ResponseEntity.status(200).body(lockerOwnerService.mylockers(email));
+        }catch(Exception e){
+            return ResponseEntity.status(400).body("에러발생");
         }
-        return ResponseEntity.status(200).body(lockerOwnerService.mylockers(email));
 
     }
 }
