563a5e66ad80a3ed7ad9903aeb4543acba8d20c3
choijinhap <pppooo95@naver.com>
2022-09-23 15:06:46 +0900

    fix - 오타 수정

--- client/src/constant/locks.ts
+++ client/src/constant/locks.ts
@@ -1,11 +1,11 @@
 const locks = [
   {
     lock_design: 1,
-    image_src: '/images/lock/1.png',
+    image_src: '/images/locks/1.png',
   },
   {
     lock_design: 2,
-    image_src: '/images/lock/2.png',
+    image_src: '/images/locks/2.png',
   },
 ];
 
