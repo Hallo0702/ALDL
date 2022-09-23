0b99da4878495d639723508c7a2612c7082978ad
choijinhap <pppooo95@naver.com>
2022-09-23 14:59:54 +0900

    feat - 자물쇠 이미지 추가 및 constant로 관리

---
+++ client/src/constant/locks.ts
@@ -0,0 +1,12 @@
+const locks = [
+  {
+    lock_design: 1,
+    image_src: '/images/lock/1.png',
+  },
+  {
+    lock_design: 2,
+    image_src: '/images/lock/2.png',
+  },
+];
+
+export default locks;
