5b691ca04d07fbecdd0366d8b4ef692091d1623b
choijinhap <pppooo95@naver.com>
2022-09-25 20:45:21 +0900

    feat - Forminput forwardRef 추가

--- client/src/components/common/FormInput.tsx
+++ client/src/components/common/FormInput.tsx
@@ -1,4 +1,4 @@
-import type { FC } from 'react';
+import { FC, forwardRef } from 'react';
 
 interface FormInputProps extends React.ComponentPropsWithoutRef<'input'> {
   label?: string;
@@ -7,36 +7,33 @@ interface FormInputProps extends React.ComponentPropsWithoutRef<'input'> {
   errMsg?: string;
 }
 
-const FormInput: FC<FormInputProps> = ({
-  label,
-  id,
-  isError,
-  errMsg,
-  onChange,
-  ...rest
-}) => {
-  return (
-    <div className="text-2xl font-bold pb-12 flex h-28">
-      <div className="self-center w-48 text-right">
-        {label && (
-          <label className="mr-8 h-12" htmlFor={id}>
-            {label}
-          </label>
-        )}
+const FormInput = forwardRef<HTMLInputElement, FormInputProps>(
+  ({ label, id, isError, errMsg, onChange, ...rest }, ref) => {
+    return (
+      <div className="text-2xl font-bold pb-12 flex h-28">
+        <div className="self-center w-48 text-right">
+          {label && (
+            <label className="mr-8 h-12" htmlFor={id}>
+              {label}
+            </label>
+          )}
+        </div>
+        <div>
+          <input
+            className="w-112 h-12 rounded-full text-center p-8"
+            type="text"
+            id={id}
+            onChange={onChange}
+            ref={ref}
+            {...rest}
+          />
+          {isError && (
+            <p className="font-regular text-base text-red p-2 ml-4">{errMsg}</p>
+          )}
+        </div>
       </div>
-      <div>
-        <input
-          className="w-112 h-12 rounded-full text-center p-8"
-          type="text"
-          id={id}
-          onChange={onChange}
-          {...rest}
-        />
-        {isError && (
-          <p className="font-regular text-base text-red p-2 ml-4">{errMsg}</p>
-        )}
-      </div>
-    </div>
-  );
-};
+    );
+  }
+);
+FormInput.displayName = 'FormInput';
 export default FormInput;
