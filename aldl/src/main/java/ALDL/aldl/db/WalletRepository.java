d02c71092993821af5980234824c2160ad2dd25c
홍찬기 <sal0203@naver.com>
2022-09-29 13:40:04 +0900

    Feat/bc/issue 130

--- aldl/src/main/java/ALDL/aldl/db/WalletRepository.java
+++ aldl/src/main/java/ALDL/aldl/db/WalletRepository.java
@@ -2,11 +2,11 @@ package ALDL.aldl.db;
 
 import ALDL.aldl.model.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
-
-import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Repository
 public interface WalletRepository extends JpaRepository<Wallet,Long> {
+
+    Optional<Wallet> findWalletByAddress(String address);
 }
