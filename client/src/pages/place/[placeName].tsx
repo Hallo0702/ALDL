00446a46f1c21d8b968c3f8c81cd240bd61c60d4
최진합 <pppooo95@naver.com>
2022-09-29 10:35:51 +0900

    feat - 장소별 자물쇠 얻는 API 추가

--- client/src/pages/place/[placeName].tsx
+++ client/src/pages/place/[placeName].tsx
@@ -2,6 +2,8 @@ import { NextPage } from 'next';
 import dynamic from 'next/dynamic';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { getLocksByBackground } from '../../api/lock';
 import places from '../../constant/places';
 
 const DynamicContainer = dynamic(
@@ -18,17 +20,23 @@ const PlaceName: NextPage = ({}) => {
     { lockType: 1, top: 0, left: 0 },
     { lockType: 0, top: 15, left: 50 },
   ];
-
+  useEffect(() => {
+    const fetch = async () => {
+      const res = getLocksByBackground(place.id);
+      console.log(res);
+    };
+    fetch();
+  }, [place]);
   return (
     <>
       <Head>
         <title>{place?.name}</title>
-        <meta name="description" content={`알록달록 ${place?.name}`} />
+        <meta name="description" content={`알록달록 ${place.name}`} />
         <link rel="icon" href="/images/logo.png" />
       </Head>
       <DynamicContainer
-        bgHeight={place?.height}
-        bgWidth={place?.width}
+        bgHeight={place.height}
+        bgWidth={place.width}
         locks={locks}
         placeId={place.id}
       ></DynamicContainer>
