7c14f3a43a0405b5d230ed56aff30abf5d513b0e
choijinhap <pppooo95@naver.com>
2022-10-01 15:07:58 +0900

    feat - 자물쇠 소유 API 추가

--- client/src/api/lock.ts
+++ client/src/api/lock.ts
@@ -30,3 +30,8 @@ export const setLocker = async (body: {
   const res = await API.post('/setlocker', body);
   return res;
 };
+
+export const saveLocker = async (lockerHash: string) => {
+  const res = await API.post('/savelocker', { lockerHash });
+  return res;
+};
