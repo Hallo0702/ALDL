5de395a8d420d6a0736484a26224d4fef5371531
choijinhap <pppooo95@naver.com>
2022-10-01 17:47:44 +0900

    fix - retrieve할 떄 address 제거

--- client/src/utils/contract.ts
+++ client/src/utils/contract.ts
@@ -8,9 +8,9 @@ const web3 = new Web3(
   new Web3.providers.HttpProvider(process.env.NEXT_PUBLIC_BLOCKCHAIN_URI || '')
 );
 
-export const retrieve = async (hash: string, walletAddress: string) => {
+export const retrieve = async (hash: string) => {
   const contract = new web3.eth.Contract(ABI as AbiItem[], hash);
-  const res = await contract.methods.retrieve().call({ from: walletAddress });
+  const res = await contract.methods.retrieve().call();
   const { 0: imageSrc, 1: title, 2: content, 3: background, 4: lockType } = res;
   return { imageSrc, title, content, background, lockType };
 };
